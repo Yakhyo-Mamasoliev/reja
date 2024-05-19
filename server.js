281211167f1fa5a2539ed3dd09df30cbd4ba8573
Yakhyo <yakhyodsu@gmail.com>
2024-05-19 11:50:12 +0900

    Port changed to 3002

--- server.js
+++ server.js
@@ -1,75 +1,41 @@
-// express - backend server quirsh uchun framework
-
-
-console.log("Web Serverni boshlash");
 const express = require("express");
-const app = express(); //app ilovasini chaqiradi
+const app = express();
 const http = require("http");
 const fs = require("fs");
 
+console.log("Web Serverni boshlash");
+
 fs.readFile("database/user.json", "utf8", (err, data) => {
     if (err) {
-        console.log("ERROR:", err);
+        console.error("ERROR:", err);
     } else {
         user = JSON.parse(data);
     }
 });
 
-// 4ta bosqichi bor
-
-// 1 Kirish Code 
-// har qanday browser keladigan request uchun public folder ochiq(uni ko`roliydi);
-// keyinroq bu public folderiga css imagelarni yuklaymiz
 app.use(express.static("public"));
-app.use(express.json()); // json formatni objectga o`girib beradi.
-app.use(express.urlencoded({extended: true})); // html forumdan publish qilingan narsalarni express serverimiz qabul qilib oladi
-
-//2: Seession code
-// Sessionga bog`liq narsa yoziladi
-
-// 3 View Code
-// BSSR Backend Side Server Rendering
-// Backendda frontni (html) yasab htmlga yuboramiz
-// tp dp that we need to install ejs
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.set("views", "views");
-app.set("view engine", "ejs"); // BSSR
+app.set("view engine", "ejs");
 
-
-// 4 Rooting code
-// app.get("/hello", function(req, res) {
-//     res.end("<h1>Hello World</h1>"); // "/hello" adresni shakllantiradi, local 300 dan keiyn slash vilan helloni tayp qilamiz
-// });
-// app.get("/gift", function(req, res) {
-//     res.end("<h1>Sov`g`a bo`limi</h1>"); 
-// });
-
-app.post("/create-item", (req, res) => { // mutation qiladi, datani update qiladi
+app.post("/create-item", (req, res) => {
     console.log(req.body);
     res.json({ test: "success" });
 });
 
 app.get("/author", (req, res) => {
-	res.render("author", { user: user });
+    res.render("author", { user: user });
 });
 
-app.get("/", function (req, res) {  // serverdan data chaqirvolish
+app.get("/", (req, res) => {
     res.render("harid");
 });
 
-
-// make server
 const server = http.createServer(app);
-let PORT = 3000; // 3000 meni eski serveri edi, 3000da hato berdi
-server.listen(PORT, function (){
+const PORT = 3002;
+
+server.listen(PORT, () => {
     console.log(`The server is working successfully on port: ${PORT}`);
 });
-
-// localhost:3000 meni serverim
-// after changing the code, restart the code. 
-// gitni initialize nega kerak, boshqa odam git install deb yozganda bizni dependencidagi packagelarni install qilib beradi
-
-// master branch => for learning porpose, for us just. (later see again )
-// develop branch 
-
-
