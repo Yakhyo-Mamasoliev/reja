fc57967b79ee5efe496553ed7d77b6be152aa5cb
Yakhyo <yakhyodsu@gmail.com>
2024-05-21 22:13:30 +0900

    Task A is solved

--- train.js
+++ train.js
@@ -1,1 +1,22 @@
-console.log("Train started");
+// TASK A
+// Harf sifatida kiritilgan birinchi parametr, 
+// kiritilgan ikkinchi parametr tarkibida nechta ekanligini qaytaruvchi
+// Funktsiya tuzing
+
+// Masalan: countLetter("e", "engineer")
+// 'engineer' so'zi tarkibida 'e' harfi 3 marotaba takrorlanganligi uchun
+// 3 sonini qaytaradi
+
+function countLetter(letter, word) {
+    let count = 0;
+
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] === letter) {
+            count++;
+        }
+    }
+    // console.log(letter, "is repeated in the word", word, count, "times...");
+    return count;
+}
+
+console.log(countLetter("e", "engineer")); 
