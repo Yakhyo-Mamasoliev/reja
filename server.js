029ecd1bcbe48e3c5532b0c988a513c6afb89838
Yakhyo-Mamasoliev <yakhyodsu@gmail.com>
2024-05-24 17:52:03 +0900

    feat: mongoDB ulanish

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ mongodb.connect(connectionString, {
     if(err) console.log("ERROR on connection MongoDB");
     else {
         console.log("MongoDB connection succeed");
-        console.log(client);
+        module.exports = client;
         const app = require ("./app");
         const server = http.createServer(app);
         let PORT = 3000; 
