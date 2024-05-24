158b62e0c831f4cea4cba830c308625c682693ae
Yakhyo-Mamasoliev <yakhyodsu@gmail.com>
2024-05-24 19:18:30 +0900

    Revert "feat: mongoDB ulanish"
    This reverts commit 029ecd1bcbe48e3c5532b0c988a513c6afb89838.

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ mongodb.connect(connectionString, {
     if(err) console.log("ERROR on connection MongoDB");
     else {
         console.log("MongoDB connection succeed");
-        module.exports = client;
+        console.log(client);
         const app = require ("./app");
         const server = http.createServer(app);
         let PORT = 3000; 
