1cedd13e47046a8de41a27940d5c395ede69c872
Yakhyo-Mamasoliev <yakhyodsu@gmail.com>
2024-05-24 20:38:49 +0900

    lesson 25

--- app.js
+++ app.js
@@ -1,27 +1,55 @@
-console.log("Web Serverni boshlash");
+console.log("Web Serverni boshlash!");
 const express = require("express");
-const res = require("express/lib/response");
 const app = express();
+const fs = require("fs");
 
-// MongoDB ulash
+let user;
+
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("err here", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
+
 const db = require("./server").db();
 
-// Kirish code
 app.use(express.static("public"));
-app.use(express.json()); 
-app.use(express.urlencoded({extended: true})); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// 2: Seession code
-// 3: Views Code
+// 2: Session code
+
+// 3: Views code
 app.set("views", "views");
-app.set("view engine", "ejs"); 
+app.set("view engine", "ejs");
 
-// 4 Rooting code
-app.post("/create-item", (req, res) => { 
-  //
+// 4: Routing code
+
+app.post("/create-item", (req, res) => {
+  const new_reja = req.body.reja;
+  console.log(req.body);
+  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+    if (err) console.log(err);
+    else {
+      res.end("success");
+    }
+  });
 });
 
-app.get("/", function (req, res) { 
-    res.render("reja");
+app.get("/", function (req, res) {
+  console.log("users entered /");
+  db.collection("plans")
+    .find()
+    .toArray((err, data) => {
+      if (err) {
+        console.log(err);
+        res.end("something went wrong");
+      } else {
+        res.render('reja', {items: data})
+      }
+    });
 });
 
+module.exports = app;
