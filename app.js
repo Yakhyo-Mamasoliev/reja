b7325aed2c619ac8b12d74cc5c8505bfa5edb5e6
Yakhyo-Mamasoliev <yakhyodsu@gmail.com>
2024-05-26 04:27:58 +0900

    ochirish tugmasi ishga tushdi

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
 console.log("Web Serverni boshlash!");
-const { log } = require("console");
 const express = require("express");
 const app = express();
 const fs = require("fs");
 
-let user;
-
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("err here", err);
-  } else {
-    user = JSON.parse(data);
-  }
-});
-
-// MongoDB connect
-
+// MongoDB chaqirish
 const db = require("./server").db();
 const mongodb = require("mongodb");
 
@@ -25,36 +13,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // 2: Session code
-
 // 3: Views code
 app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4: Routing code
-
 app.post("/create-item", (req, res) => {
-  console.log("user entered / create-item");
   const new_reja = req.body.reja;
+  console.log(req.body);
   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-    console.log(data.ops);
     res.json(data.ops[0]);
   });
 });
 
-app.post("/delete-item", (req, res) => {
-  const id = req.body.id;
-  console.log(id);
-  db.collection("plans").deleteOne(
-    { _id: new mongodb.ObjectId(id) },
-    (err, data) => {
-      res.json({ state: "success" });
-    }
-  );
-});
-
-app.get("/author", (req, res) => {
-  res.render("author", { user: user });
-});
+  app.post("/delete-item", (req, res) => {
+    const id = req.body.id;
+    db.collection("plans").deleteOne({_id: new mongodb.OnjectId(id)}, function(err, data) {
+      res.json({state: "success"});
+    })    
+  })
 
 app.get("/", function (req, res) {
   console.log("users entered /");
@@ -65,9 +42,10 @@ app.get("/", function (req, res) {
         console.log(err);
         res.end("something went wrong");
       } else {
-        res.render("reja", { items: data });
+        res.render('reja', {items: data})
       }
     });
 });
 
 module.exports = app;
+
