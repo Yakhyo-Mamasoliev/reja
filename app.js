d40a99e4cb5ad22f1c8f4ebc9d4aac4e5b4bdc3f
Yakhyo-Mamasoliev <yakhyodsu@gmail.com>
2024-05-26 18:40:52 +0900

    endi o`chiradi

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.post("/create-item", (req, res) => {
 
   app.post("/delete-item", (req, res) => {
     const id = req.body.id;
-    db.collection("plans").deleteOne({_id: new mongodb.OnjectId(id)}, function(err, data) {
+    db.collection("plans").deleteOne({_id: new mongodb.ObjectId(id)}, function(err, data) {
       res.json({state: "success"});
     })    
   })
